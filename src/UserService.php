3c710032c5a58d9a9d34429b5178722140dd11bc
dongjiawei <djwjiawei@163.com>
2020-02-09 19:06:46 +0800

    'composer包测试'

---
+++ src/UserService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: djwji
+ * Date: 2020/2/9
+ * Time: 16:24
+ */
+namespace Dapi;
+
+class UserService{
+    public function index(){
+        echo 'user version 2 test 123';
+    }
+}
